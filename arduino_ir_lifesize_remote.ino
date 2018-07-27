887d1c38d97def157a0aec3530058b5c9002acbd
Reza Pakdel <rpakdel@gmail.com>
2018-07-27 00:58:12 -0700

    Decoding implementation

--- arduino_ir_lifesize_remote.ino
+++ arduino_ir_lifesize_remote.ino
@@ -1,26 +1,114 @@
 #include <IRremote.h>
+#include "lifesize-buttons.h"
 
-#define IR_PIN 6
-IRrecv irrecv(IR_PIN);
+#define RECEIVER_PIN 6
+IRrecv irrecv(RECEIVER_PIN);
 
 void setup()
 {
-	Serial.begin(9600);   // Status message will be sent to PC at 9600 baud
-	irrecv.enableIRIn();  // Start the receiver
+    Serial.begin(9600);
+    irrecv.enableIRIn();
+    Serial.println("Press a button");
 }
 
-void printIRButton(decode_results& irresults)
+void printButton(Button button)
 {
-	Serial.println(irresults.value, HEX);
+    int buttonIndex = (int)button;
+    Serial.print(buttonLabel[buttonIndex]);
 }
 
-void loop()
+void printlnButton(Button button)
 {
-	decode_results irresults;
-	if (irrecv.decode(&irresults))
-	{
-		printIRButton(irresults);
-		irrecv.resume();
-	}
+    int buttonIndex = (int)button;
+    Serial.println(buttonLabel[buttonIndex]);
+}
 
+Button getButton(unsigned int code)
+{
+    for (int i = 0; i < NUM_BUTTONS; i++)
+    {
+        if (code == codes1[i] || code == codes2[i])
+        {
+            return Button(i);
+        }
+    }
+    return Undefined;
+}
+
+unsigned int decodeIR(decode_results *results)
+{
+    byte result[16];
+
+    for (int k = 0; k < 16; k++)
+    {
+        result[k] = 0;
+    }
+
+    if (results->rawlen != 36)
+    {
+        //Serial.print("Wrong number of pulses");
+        //Serial.println(results->rawlen);
+        return (int)codes1[Invalid];
+    }
+
+
+    int bit_index = 0;
+
+    // pulse 0 is time since last pulse
+    // pulse 1 and 2 are start
+    // last pulse is stop
+    // we only look at even pulses
+    int len = results->rawlen - 1;
+    for (int i = 3; i < len; i = i + 2)
+    {
+        // current pulse
+        unsigned long  x = results->rawbuf[i] * USECPERTICK;
+        // next pulse
+        unsigned long  n = results->rawbuf[i + 1] * USECPERTICK;
+
+        // print timing
+        //Serial.print(x);
+        //Serial.print("-");
+        //Serial.println(n);
+        if (x >= 1000 && x <= 2000)
+        {
+            // low, check next pulse
+            if (n >= 2000 && n < 3000)
+            {
+                // next pulse is long, got a 1
+                result[bit_index] = 1;
+            }
+            else if (n < 1000)
+            {
+                result[bit_index] = 0;
+            }
+        }
+        bit_index++;
+    }
+
+    unsigned int value = 0;
+    for (int j = 0; j < 16; j++)
+    {
+        // print value in binary
+        // Serial.print(result[j]);
+        value |= result[j] << (15 - j);
+    }
+    //Serial.println();
+    //Serial.println(value, HEX);
+    return value;
+}
+
+void  loop()
+{
+    decode_results results;
+    if (irrecv.decode(&results))
+    {
+        unsigned int code = decodeIR(&results);
+        Serial.print(code, HEX);
+        Serial.print(" -> ");
+        Button button = getButton(code);
+        //Serial.println(button);
+        printlnButton(button);
+        irrecv.resume();
+    }
 }
